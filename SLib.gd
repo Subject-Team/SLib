73a1951678cc2c4d70487d0510d43c843a77d97a
Mahan Khalili <khalili1388mahan@gmail.com>
2024-08-30 10:06:21 +0330

    Revert "apply style guide"
    This reverts commit ad1280ae3dcd3dae3fee9f9b7a29a30bc2ee875b.

--- SLib.gd
+++ SLib.gd
@@ -28,34 +28,36 @@ extends Node
 ## [br]
 ## NOTE: Use [code]Project > Project Settings > SLib[/code] for change defaults and file locations.
 
-#region config
 ## Default values
-var defaults: Dictionary = {
+var Defaults: Dictionary = {
 	"Descendants": false,
 	"AlertTitle": "Alert!",
 	"Error": "Error",
 	"Warning": "Warning",
 	"BackupSuffix": "Backup",
-	"ScenesFolder": "Scene",
-}
+	"ScenesFolder": "Scene"
+	}
 
 ## Library file locations
-var file_locations: Dictionary = {
-	"Log": "user://App.log",
+var FileLocations: Dictionary = {
+	"Log": "user://App.log"
 }
-#endregion
 
-#region private variables
-var _user_pattern: Array
-#endregion
+## Global variable for [method SLibMain.CustomSort]
+var UserPattern: Array
 
-#region initalizing
 func _enter_tree():
-	defaults = ProjectSettings.get_setting("SLib/Defaults", defaults)
-	file_locations = ProjectSettings.get_setting("SLib/FileLocations", file_locations)
-#endregion
-
-#region main
+	Defaults = ProjectSettings.get_setting("SLib/Defaults", {
+		"Descendants": false,
+		"AlertTitle": "Alert!",
+		"Error": "Error",
+		"Warning": "Warning",
+		"BackupSuffix": "Backup",
+		"ScenesFolder": "Scene"
+		})
+	FileLocations = ProjectSettings.get_setting("SLib/FileLocations", {
+		"Log": "user://App.log"
+	})
 
 ## You can use this function to transition between scenes, this increases code readability and helps you understand which scene in the target.
 ## [br][br]
@@ -91,47 +93,41 @@ func _enter_tree():
 ## [/codeblock]
 ## NOTE:
 ## You can also call nested folders, for example: [code]Scenes/Old Files[/code]
-func change_scene(scene_name: String, folder: String = defaults["ScenesFolder"]) -> void:
-	if folder == "/root":
-		get_tree().change_scene_to_file("res://" + scene_name + ".tscn")
+func GoToScene(SceneName: String, Folder: String = Defaults["ScenesFolder"]) -> void:
+	if Folder == "/root":
+		get_tree().change_scene_to_file("res://" + SceneName + ".tscn")
 	else:
-		get_tree().change_scene_to_file("res://" + folder + "/" + scene_name + ".tscn")
-
+		get_tree().change_scene_to_file("res://" + Folder + "/" + SceneName + ".tscn")
 
 ## This function changes the project settings, for this it needs two parameters:
 ## [br]
 ## ● Path: can use [kbd]Ctrl+Shift+C[/kbd] on property of project settings window
 ## [br]
 ## ● Value: this parameter set to selected property
-func set_project_setting(path: String, value) -> void:
-	ProjectSettings.set_setting(path, value)
-
+func SetProjectSetting(Path: String, Value) -> void:
+	ProjectSettings.set_setting(Path, Value)
 
 ## This function returns the variable stored in the desired property, its use is as follows:
 ## [codeblock]
 ## MyVar = SLib.GetPrjectSetting("application/config/windows_native_icon")
 ## [/codeblock]
-func get_project_setting(path: String):
-	return ProjectSettings.get_setting(path)
-
+func GetProjectSetting(Path: String):
+	return ProjectSettings.get_setting(Path)
 
 ## This function reloads the current scene, this is just a shortcut to increase code readability.
-func reload() -> void:
+func Reload() -> void:
 	get_tree().reload_current_scene()
 
-
 ## This function closes the program and also uses the optional exit code, added for code readability.
-func exit(exit_code: int = 0) -> void:
-	get_tree().quit(exit_code)
-
+func Exit(ExitCode: int = 0) -> void:
+	get_tree().quit(ExitCode)
 
 ## This function creates a break in the program that puts a space between the code befor and after it. (on seconds)
 ## [br][br]
 ## NOTE:
 ## You must write [code]await[/code] at the beginning of this line for it to work!
-func wait(wait_time: float) -> void:
-	await get_tree().create_timer(wait_time).timeout
-
+func Wait(WaitTime: float) -> void:
+	await get_tree().create_timer(WaitTime).timeout
 
 ## This function will save a file with a customized path, this is very useful because the file saving process will be readable and fast.
 ## [br][br]
@@ -143,58 +139,52 @@ func wait(wait_time: float) -> void:
 ## [br][br]
 ## NOTE:
 ## You should type a format for your file, you can select any format but default format not open in other programs!
-func save_file(location: String, value = null) -> void:
-	var file = FileAccess.open(location,FileAccess.WRITE)
-	file.store_var(value)
+func SaveFile(Location: String, Variable = null) -> void:
+	var file = FileAccess.open(Location,FileAccess.WRITE)
+	file.store_var(Variable)
 	file.close()
-
 
 ## This function returns the content stored in the file, you can use it for all file created by your godot application.
 ## [br][br]
 ## NOTE:
-## If the file doesn't exist, it will send an error to the console like this: [code]SLib.gd:x @ SendError(): SLib File Loader: Can't load from -->file_location<--, file not exists![/code]
-func load_file(location: String):
-	if FileAccess.file_exists(location):
-		var file = FileAccess.open(location,FileAccess.READ)
+## If the file doesn't exist, it will send an error to the console like this: [code]SLib.gd:x @ SendError(): LoadFile(): Can't load from -->file_location<--, file not exists![/code]
+func LoadFile(Location: String):
+	if FileAccess.file_exists(Location):
+		var file = FileAccess.open(Location,FileAccess.READ)
 		var data = file.get_var()
 		file.close()
 		return data
 	else:
-		send_error("Can't load from " + location + ", file not exists!", "SLib File Loader")
+		SendError("Can't load from " + Location + ", file not exists!", "LoadFile()")
 
-
-## Backup function create a new file with [code]main file name-suffix[/code] in main file location, if you doesn't select a custom suffix, [code]-Backup[/code] append to file name.
+## Backup function create a new file with [code]main file name-Suffix[/code] in main file location, if you doesn't select a custom suffix, [code]-Backup[/code] append to file name.
 ## [br][br]
 ## NOTE:
-## If the file doesn't exist, it will send an error to the console like this: [code]SLib.gd:x @ SendError(): SLib File Backup: Can't load from -->file_location<--, file not exists![/code]
+## If the file doesn't exist, it will send an error to the console like this: [code]SLib.gd:x @ SendError(): BackupFile(): Can't load from -->file_location<--, file not exists![/code]
 ## [br][br]
-func backup_file(location: String, suffix: String = defaults["BackupSuffix"]) -> void:
-	if FileAccess.file_exists(location):
-		var file = FileAccess.open(location,FileAccess.READ)
+func BackupFile(Location: String, Suffix: String = Defaults["BackupSuffix"]) -> void:
+	if FileAccess.file_exists(Location):
+		var file = FileAccess.open(Location,FileAccess.READ)
 		var data = file.get_var()
-		var backup_location = location.get_basename() + "-" + suffix + "." + location.get_extension()
-		var backup = FileAccess.open(backup_location,FileAccess.WRITE)
+		var Backup_Location = Location.get_basename() + "-" + Suffix + "." + Location.get_extension()
+		var backup = FileAccess.open(Backup_Location,FileAccess.WRITE)
 		backup.store_var(data)
 		file.close()
 		backup.close()
 	else:
-		send_error("Can't load from " + location + ", file not exists!", "SLib File Backup")
-
+		SendError("Can't load from " + Location + ", file not exists!", "BackupFile()")
 
 ## Sends a custom error to the console that can be viewed in the engine debugger, error like this: [code]SLib.gd:x @ SendError(): ->From<-: ->Error<-[/code]
-func send_error(error: String = defaults["Error"], from: String = "Debugger") -> void:
-	push_error(from + ": " + error)
-
+func SendError(Error: String = Defaults["Error"], From: String = "Debugger") -> void:
+	push_error(From + ": " + Error)
 
 ## Sends a custom warning to the console that can be viewed in the engine debugger like this: [code]SLib.gd:x @ SendWarning(): ->From<-: ->Warning<-[/code]
-func send_warning(warning: String = defaults["Warning"], from: String = "Debugger") -> void:
-	push_warning(from + ": " + warning)
-
+func SendWarning(Warning: String = Defaults["Warning"], From: String = "Debugger") -> void:
+	push_warning(From + ": " + Warning)
 
 ## Displays a modal dialog box using the host OS' facilities with alert text and title.
-func send_alert(alert: String, title: String = defaults["AlertTitle"]) -> void:
-	OS.alert(alert, title)
-
+func SendAlert(Alert: String, Title: String = Defaults["AlertTitle"]) -> void:
+	OS.alert(Alert, Title)
 
 ## Save log parameter in log file, log file save in [code]user://Log.ject[/code] as default.
 ## [br][br]
@@ -203,14 +193,12 @@ func send_alert(alert: String, title: String = defaults["AlertTitle"]) -> void:
 ## [br][br]
 ## NOTE:
 ## You can change the log file path with [code]SLib.Log_FileLcation[/code].
-func save_log(custom_log) -> void:
-	save_file(file_locations["Log"], custom_log)
-
+func SaveLog(Log) -> void:
+	SaveFile(FileLocations["Log"], Log)
 
 ## Return saved log.
-func get_log():
-	return load_file(file_locations["Log"])
-
+func GetLog():
+	return LoadFile(FileLocations["Log"])
 
 ## Requests the OS to open a resource with the most appropriate program. For example:
 ## [br][br]
@@ -228,24 +216,23 @@ func get_log():
 ## [br][br]
 ## NOTE:
 ## This method is implemented on Android, iOS, Web, Linux, macOS and Windows.
-func os_open(uri: String) -> void:
-	OS.shell_open(uri)
-
+func OSOpen(URI: String) -> void:
+	OS.shell_open(URI)
 
 ## Returns the absolute, native OS path corresponding to the localized path (starting with [code]res://[/code] or [code]user://[/code]).
 ## The returned path will vary depending on the operating system and user preferences.
 ## See [url=https://docs.godotengine.org/en/4.2/tutorials/io/data_paths.html]File paths in Godot projects[/url] to see what those paths convert to.
-func full_path(path: String) -> String:
-	if path[0] == "u":
-		return ProjectSettings.globalize_path(path)
+func FullPath(Path: String) -> String:
+	if Path[0] == "u":
+		return ProjectSettings.globalize_path(Path)
 	else:
+		var path = Path
 		if OS.has_feature("editor"):
-			path = ProjectSettings.globalize_path(path)
+			path = ProjectSettings.globalize_path(Path)
 		else:
 			path = path.erase(0,6)
 			path = OS.get_executable_path().get_base_dir().path_join(path)
 		return path
-
 
 ## It combines two arrays and only adds items from the second array to the first array if the first array does not already contain them.
 ## [br][br]
@@ -253,38 +240,39 @@ func full_path(path: String) -> String:
 ## [codeblock]
 ## var myarray1: Array = [1,2,3,4]
 ## var myarray2: Array = [3,4,5,6]
-## var merged_array: Array = SLib.merge_unique(myarray1,myarray2)
+## var merged_array: Array = SLib.MergeUnique(myarray1,myarray2)
 ## print(merged_array) # prints [1,2,3,4,5,6]
 ## [/codeblock]
-func merge_unique(array1: Array, array2: Array) -> Array:
-	var merged_array = []
-	for i in array1:
-		if not merged_array.has(i):
-			merged_array.append(i)
-	for j in array2:
-		if not merged_array.has(j):
-			merged_array.append(j)
-	return merged_array
+## NOTE: Removing duplicate values ​​in the first array is determined by the FullUnique parameter, set it to [code]true[/code] for optimize all array items.
+func MergeUnique(Array1: Array, Array2: Array) -> Array:
+	var MergedArray = []
+	for Array1Item in Array1:
+		if not MergedArray.has(Array1Item):
+			MergedArray.append(Array1Item)
+	for Array2Item in Array2:
+		if not MergedArray.has(Array2Item):
+			MergedArray.append(Array2Item)
+	return MergedArray
 
-
+## @experimental
 ## Finds the first child of a given class, does not find class_name declarations
-func find_child_of_class(target_node: Node, type_name: StringName, descendants: bool = defaults["Descendants"]) -> Node:
-	for child in target_node.get_children():
-		if child.is_class(type_name):
-			return child
-		elif descendants:
-			var found: Node = find_child_of_class(child, type_name, descendants)
-			if found:
-				return found
+func FindChildOfClass(TargetNode: Node, TypeName: StringName, Descendants: bool = Defaults["Descendants"]) -> Node:
+	for Child in TargetNode.get_children():
+		if Child.is_class(TypeName):
+			return Child
+		elif Descendants:
+			var Found: Node = FindChildOfClass(Child, TypeName, Descendants)
+			if Found:
+				return Found
 	return null
 
-
+## @experimental
 ## Time based smooth interpolation, [b]not framedependant[/b] like [code]a = lerp(a, b, delta)[/code][br][br]
-## NOTE: Set [i]decay[/i] to 1-25 for a good range of values
-func exp_decay(a: float, b: float, decay: float, delta: float) -> float:
-	return b + (a - b) * exp(-decay * delta)
+## NOTE: Set [i]Decay[/i] to 1-25 for a good range of values
+func expDecay(A: float, B: float, Decay: float, Delta: float) -> float:
+	return B + (A - B) * exp(-Decay * Delta)
 
-
+## @experimental
 ## Auto Change get_tree().paused, if true, the SceneTree is paused. Doing so will have the following behavior:
 ## [br][br]
 ## - 2D and 3D physics will be stopped. This includes signals and collision detection.
@@ -293,58 +281,51 @@ func exp_decay(a: float, b: float, decay: float, delta: float) -> float:
 ## [br][br]
 ## If you need fast change, only use [code]PauseChange()[/code]. When need to set pause, use [code]ChagePause(true)[/code] or [code]ChagePause(false)[/code].
 ## [br][br]
-## NOTE: If you set a not-boolean parameter, it will send an error to the console: [code]SLib.gd:x @ SendError(): SLib Pause Changer: Only use boolean parameters![/code]
-func change_pause(pause = null) -> void:
-	if pause == null:
+## NOTE: If you set a not-boolean parameter, it will send an error to the console: [code]SLib.gd:x @ SendError(): ChangePause(): Only use boolean parameters![/code]
+func PauseChange(Pause = null):
+	if Pause == null:
 		get_tree().paused = !get_tree().paused
-	elif typeof(pause) == 1:
-		get_tree().paused = pause
+	elif typeof(Pause) == 1:
+		get_tree().paused = Pause
 	else:
-		send_error("Only use boolean parameters!", "SLib Pause Changer")
-
+		SendError("Only use boolean parameters!", "PauseChange()")
 
 ## This function shows an object and creates an animation to change its color.
-func appear(object) -> void:
+func Appear(object) -> void:
 	object.show() 
 	create_tween().tween_property(object, "modulate", Color.WHITE, 1.0)
 
 ## This function creates an animation to make an object disappear by changing its color to transparent.
-func disappear(object) -> void:
+func Disappear(object) -> void:
 	var tween = create_tween()
 	tween.tween_property(object, "modulate", Color.TRANSPARENT, 1.0)
 	tween.finished.connect(func(): object.hide())
 
-
+## @experimental
 ## Reverses the key-value pairs in a given dictionary.
-func reverse_dict(dict: Dictionary) -> Dictionary:
-	var reverse: Dictionary = {}
-	for key in dict.keys():
-		reverse[dict[key]] = key
-	return reverse
-
+func ReverseDict(Dict: Dictionary):
+	var Reverse: Dictionary = {}
+	for Key in Dict.keys():
+		Reverse[Dict[Key]] = Key
+	return Reverse
 
 ## Sorts the array based on the sorter, eg:
 ## [codeblock]
 ## var Scrambled = ["5", "A", "10", "K", "J", "Q", "3"]
 ## var Pattern = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 ##
-## var Sorted = SLib.patterned_sort(Scrambled, Pattern) #-> ["A", "3", "5", "10", "J", "Q", "K"] 
+## var Sorted = SLib.CustomSort(Scrambled, Pattern) #-> ["A", "3", "5", "10", "J", "Q", "K"] 
 ## [/codeblock]
-func patterned_sort(scrambled_array : Array, pattern: Array) -> Array:
-	_user_pattern = pattern
-	scrambled_array.sort_custom(_sorter)
-	var sorted_array = scrambled_array
-	return sorted_array
+func CustomSort(ScrambledArray : Array, Pattern: Array) -> Array:
+	UserPattern = Pattern
+	ScrambledArray.sort_custom(_sorter)
+	return ScrambledArray
 
-
-## Return global file locations by key.
-func file_path(key: String) -> String:
-	return file_locations[key]
-#endregion
-
-#region private functions
 func _sorter(a, b):
-	if _user_pattern.find(a) < _user_pattern.find(b):
+	if UserPattern.find(a) < UserPattern.find(b):
 		return true
 	return false
-#endregion
+
+## Return global file locations by key.
+func GetPath(Key: String) -> String:
+	return FileLocations[Key]
