66bab7d20dd6810bca9b262a6934723f2f0ec701
Mahan <khalili1388mahan@gmail.com>
2025-01-11 09:39:41 +0330

    Update globalize_path docs

--- addons/SLib/SLib.gd
+++ addons/SLib/SLib.gd
@@ -155,8 +155,10 @@ func set_file_path(key: String, path: String) -> void:
 
 #region PATH CONVERTER
 ## Returns the absolute, native OS path corresponding to the localized path (starting with [code]res://[/code] or [code]user://[/code]).
-## The returned path will vary depending on the operating system and user preferences.
-## See [url=https://docs.godotengine.org/en/4.2/tutorials/io/data_paths.html]File paths in Godot projects[/url] to see what those paths convert to.
+## The returned path will vary depending on the operating system and user preferences.[br]
+## If it receives a file in [code]"res://"[/code] in the exported project, unlike [code]ProjectSettings.globalize_path()[/code] it returns its path in the exportded accompanying files.[br]
+## See [url=https://docs.godotengine.org/en/4.2/tutorials/io/data_paths.html]File paths in Godot projects[/url] to see what those paths convert to.[br]
+## See also [method localize_path].
 func globalize_path(path: String) -> String:
 	if path[0] == "u":
 		return ProjectSettings.globalize_path(path)
