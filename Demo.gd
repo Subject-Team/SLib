e957ece517bf27ba738310e11a6eed008f28983b
Mahan <khalili1388mahan@gmail.com>
2025-01-11 09:36:00 +0330

    Fix Demo file

--- Demo.gd
+++ Demo.gd
@@ -2,15 +2,19 @@ extends Node
 
 # Demo Script For Test & Show SLib functions
 
+func _ready():
+	SLib.save_file(0, "res://test")
+
 func _backup_game():
-	"""
-	This one line of code creates a backup of the player's data file with an optional
-	extension next to it, it has the ability to check all types of errors and manage them,
-	prevent syntax errors, repeating lines and code length.
-	"""
+	# This one line of code creates a backup of the player's data file with an optional
+	# extension next to it, it has the ability to check all types of errors and manage them,
+	# prevent syntax errors, repeating lines and code length.
 	SLib.backup_json_file("user://main_data.json")
 
 func _old_backup_game():
+	# These 10 lines of code do the same thing as one line above, with the difference that
+	# the file extension and path must be specified in several places each time. 
+	# and creates variables that are no longer usable!
 	var file_access := FileAccess.open("user://main_data.json", FileAccess.READ)
 	var json_string := file_access.get_line()
 	file_access.close()
