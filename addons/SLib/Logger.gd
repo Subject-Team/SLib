ab4fd7cb0d4f73a08732c8daa53c126715add4a2
Mahan <khalili1388mahan@gmail.com>
2025-01-12 16:41:59 +0330

    Setup Logger.gd

---
+++ addons/SLib/Logger.gd
@@ -0,0 +1,23 @@
+class_name SLibLoggerDocs
+extends Node
+# Press F1 and search for SLib to see documentation
+
+# TODO
+# ---
+
+# NOTE
+# ---
+
+# WARNING
+# ---
+
+# TEST
+# ---
+
+# ALERT
+# ---
+
+enum parts {
+	GAME,
+}
+
