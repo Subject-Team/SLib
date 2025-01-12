6eb44e835df50c8a7805f83dc9ad6e046a3a15b1
Mahan <khalili1388mahan@gmail.com>
2025-01-12 16:42:22 +0330

    Add wake_up function

--- addons/SLib/Logger.gd
+++ addons/SLib/Logger.gd
@@ -21,3 +21,13 @@ enum parts {
 	GAME,
 }
 
+var awake := false
+var default_part: parts = parts.GAME
+
+func wake_up(set_default_part_to: parts = parts.GAME) -> void:
+	if awake:
+		SLib.send_warning("Waking the system was not successful, it is still awake!", "Log.wake_up")
+		return
+	awake = true
+	default_part = set_default_part_to
+
