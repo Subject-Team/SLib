151414a76dbffa2b90bf226931590869370a52cb
Mahan <khalili1388mahan@gmail.com>
2025-01-12 16:38:32 +0330

    Fix demo

--- Demo.gd
+++ Demo.gd
@@ -3,7 +3,7 @@ extends Node
 # Demo Script For Test & Show SLib functions
 
 func _ready():
-	SLib.save_file(0, "res://test")
+	pass
 
 func _backup_game():
 	# This one line of code creates a backup of the player's data file with an optional
